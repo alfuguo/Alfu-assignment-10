50ee7e2b2c65f4ae941ace63347964d34ea08010
alfuguo <137233519+alfuguo@users.noreply.github.com>
2024-04-18 14:50:10 -0400

    updating

--- src/main/java/com/coderscampus/assignment10/service/Assigment10Service.java
+++ src/main/java/com/coderscampus/assignment10/service/Assigment10Service.java
@@ -2,34 +2,39 @@ package com.coderscampus.assignment10.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 import java.util.Optional;
 
+@Service
 public class Assigment10Service {
     @Value("${spoonacular.urls.base}")
     private String spoonacularUrls;
 
-    @Value("${spoonacular.urls.mealplan}")
+    @Value("${spoonacular.urls.mealpla  n}")
     private String spoonacularMealplan;
 
     @Value("${spoonacular.api.key}")
-    private String spoonacularApiKey;
+    private String apiKey;
 
     RestTemplate restTemplate = new RestTemplate();
 
     public<T> ResponseEntity<T> fetchDataFromSpoonacular (String timeFrame, String numCalories, String exclude, String diet, Class<T>responseClass) {
-        URI uri = UriComponentsBuilder.fromHttpUrl(spoonacularUrls + spoonacularMealplan)
-                .queryParam("apiKey" , spoonacularApiKey)
-                .queryParam("timeFrame", timeFrame)
-                .queryParam("targetCalories" , Optional.ofNullable(numCalories))
-                .queryParam("exclude" , Optional.ofNullable(exclude))
-                .queryParam("diet" , Optional.ofNullable(diet))
-                .build()
-                .toUri();
-
-    return restTemplate.getForEntity(uri, responseClass);
+        String uri = buildUri(timeFrame, numCalories, exclude, diet);
+        return restTemplate.getForEntity(uri, responseClass);
     }
+
+    private String buildUri(String timeFrame, String numCalories, String exclude, String diet) {
+        return UriComponentsBuilder.fromHttpUrl(spoonacularUrls + spoonacularMealplan )
+                .queryParam("apiKey", apiKey)
+                .queryParam("timeFrame", timeFrame)
+                .queryParamIfPresent("targetCalories", Optional.ofNullable(numCalories))
+                .queryParamIfPresent("exclude", Optional.ofNullable(exclude))
+                .queryParamIfPresent("diet", Optional.ofNullable(diet))
+                .build()
+                .toUriString();
+
+    }
+
 }
--- src/main/java/com/coderscampus/assignment10/web/Assigment10Controller.java
+++ src/main/java/com/coderscampus/assignment10/web/Assigment10Controller.java
@@ -1,15 +1,19 @@
 package com.coderscampus.assignment10.web;
-
 import com.coderscampus.assignment10.dto.DayResponse;
 import com.coderscampus.assignment10.dto.WeekResponse;
 import com.coderscampus.assignment10.service.Assigment10Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Assigment10Controller {
-    @Autowired
-    private Assigment10Service assigment10Service;
+
+    private final Assigment10Service assigment10Service;
+
+    public Assigment10Controller(Assigment10Service assigment10Service) {
+        this.assigment10Service = assigment10Service;
+    }
 
 
     @GetMapping("mealplanner/day")
